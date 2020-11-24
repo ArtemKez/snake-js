8c9db9f4ce3397260120f3623a4435ed716e2b40
ArtemKez <artem.kez.work@gmail.com>
2020-11-24 17:23:29 +0200

    20x20

--- snake.js
+++ snake.js
@@ -2,7 +2,7 @@ let field = document.createElement('div');
 document.body.appendChild(field);
 field.classList.add('field');
 
-for (let i = 1; i < 101; i++) {
+for (let i = 1; i < 401; i++) {
     let excel = document.createElement('div');
     field.appendChild(excel);
     excel.classList.add('excel');
@@ -10,10 +10,10 @@ for (let i = 1; i < 101; i++) {
 
 let excel = document.getElementsByClassName('excel');
 let x = 1,
-    y = 10;
+    y = 20;
 
-for (let i = 0; i < 100; i++) {
-    if (x > 10) {
+for (let i = 0; i < 400; i++) {
+    if (x > 20) {
         x = 1;
         y--;
     }
@@ -23,8 +23,8 @@ for (let i = 0; i < 100; i++) {
 }
 
 function generateSnake() {
-    let posX = Math.round(Math.random() * (10 - 3) + 3);
-    let posY = Math.round(Math.random() * (10 - 1) + 1);
+    let posX = Math.round(Math.random() * (20 - 3) + 3);
+    let posY = Math.round(Math.random() * (20 - 1) + 1);
     return [posX, posY];
 }
 
@@ -43,8 +43,8 @@ let mouse;
 
 function createMouse() {
     function generateMouse() {
-        let posX = Math.round(Math.random() * (10 - 3) + 3);
-        let posY = Math.round(Math.random() * (10 - 1) + 1);
+        let posX = Math.round(Math.random() * (20 - 3) + 3);
+        let posY = Math.round(Math.random() * (20 - 1) + 1);
         return [posX, posY];
     }
 
@@ -76,7 +76,28 @@ display: block;
 let score = 0;
 input.value = `Ваши очки: ${score}`;
 
+function updateMove(duration) {
+    stopMove()
+    interval = setInterval(move, duration)
+}
+
+function stopMove() {
+    if (interval) {
+        clearInterval(interval);
+        interval = null;
+    }
+}
+
+function clear() {
+    if (score > 1) {
+        interval = setInterval(move, 100);
+    }
+}
+
 function move() {
+
+
+
     let snakeCoordinates = [snakeBody[0].getAttribute('posX'), snakeBody[0].getAttribute('posY')];
 
     snakeBody[0].classList.remove('head');
@@ -84,7 +105,7 @@ function move() {
     snakeBody.pop();
 
     if (direction === 'right') {
-        if (snakeCoordinates[0] < 10) {
+        if (snakeCoordinates[0] < 20) {
             snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] + 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
         } else {
             snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'));
@@ -94,11 +115,11 @@ function move() {
         if (snakeCoordinates[0] > 1) {
             snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] - 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
         } else {
-            snakeBody.unshift(document.querySelector('[posX = "10"][posY = "' + snakeCoordinates[1] + '"]'));
+            snakeBody.unshift(document.querySelector('[posX = "20"][posY = "' + snakeCoordinates[1] + '"]'));
 
         }
     } else if (direction === 'up') {
-        if (snakeCoordinates[1] < 10) {
+        if (snakeCoordinates[1] < 20) {
             snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (+snakeCoordinates[1] + 1) + '"]'));
         } else {
             snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "1"]'));
@@ -108,7 +129,7 @@ function move() {
         if (snakeCoordinates[1] > 1) {
             snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (snakeCoordinates[1] - 1) + '"]'));
         } else {
-            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "10"]'));
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "20"]'));
         }
     }
 
@@ -143,7 +164,7 @@ function move() {
 let interval = setInterval(move, 300);
 
 window.addEventListener('keydown', function (e) {
-    const keyKode = e.key || e.keyCode;
+    const keyKode = e.key;
     if (steps === true) {
         if (keyKode === 'ArrowLeft' && direction !== 'right') {
             direction = 'left';
